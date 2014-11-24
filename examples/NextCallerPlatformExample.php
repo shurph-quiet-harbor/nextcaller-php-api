fa85fc1015fd660f7cf8fb81b719c6a2780f2f8d
Kirill Utenkov <kirill.utenkov@itechart-group.com>
2014-11-24 13:00:15 +0300

    Added examples and changed readme.md

---
+++ examples/NextCallerPlatformExample.php
@@ -0,0 +1,27 @@
+<?php
+
+require(__DIR__ . '/vendor/autoload.php');
+
+use NextCaller\NextCallerPlatformClient;
+
+$user = "";
+$password = "";
+$PROFILE_ID = 'e79ba4dab9cdd3da41c95ef734ec5b';
+$platformUser = 'user1';
+$sandbox = true;
+
+$client = new NextCallerPlatformClient($user, $password, $sandbox);
+try {
+    $profile = $client->getProfile($PROFILE_ID, $platformUser);
+    var_dump($profile);
+} catch (\NextCaller\Exception\BadResponseException $e) {
+    var_dump($e->getCode());
+    var_dump($e->getMessage());
+} catch (\NextCaller\Exception\FormatException $e) {
+    var_dump($e->getCode());
+    var_dump($e->getMessage());
+} catch (\Buzz\Exception\RequestException $e) {
+    var_dump($e->getCode());
+    var_dump($e->getMessage());
+    var_dump($e->getRequest());
+}
